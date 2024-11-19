310706c61bd43064e32bdb4ced697bd77597c815
ishyverma <ishyamver@gmail.com>
2024-11-19 15:50:37 +0530

    added login route for user

--- backend/routers/user.routes.js
+++ backend/routers/user.routes.js
@@ -3,8 +3,11 @@ import { User } from '../models/users.model.js'
 import bcrypt from 'bcrypt'
 import { upload } from '../middlewares/multer.js'
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
 export const router = Router()
+dotenv.config('../.env')
 
 router.post('/register', upload.fields([{ name: 'profilePicture', maxCount: 1 }]), async (req, res) => {
     const { username, password, email, profilePicture } = req.body
@@ -47,4 +50,31 @@ router.post('/register', upload.fields([{ name: 'profilePicture', maxCount: 1 }]
 
     console.log('Files', req.files)
     console.log('Body', req.body)
+})
+
+router.post('/login', async (req, res) => {
+    const { username, password, email } = req.body
+    if ([username, password, email].some(field => field?.trim() === '')) {
+        throw new Error('All Fields Are Required')
+    }
+    const hashedPassword = await bcrypt.compare(password)
+    if (hashedPassword) {
+        res.status(404).json({
+            ErrorMessage: 'Password Is Incorrect'
+        })
+    }
+    try {
+        const user = await User.findOne({
+            $or: [{ username }, { password }, { email }]
+        })
+        if (user) {
+            const token = jwt.sign({ username, email }, process.env.JWT_SECRET)
+            localStorage.setItem('token', token)
+            console.log(token)
+        } else {
+            throw new Error('User Not Registered')
+        }
+    } catch (err) {
+        throw new Error('Error Fetching Data From Database')
+    }
 })
