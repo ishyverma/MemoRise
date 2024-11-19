d6877c67f03f0aaa2c00765e3b253a937fe24688
ishyverma <ishyamver@gmail.com>
2024-11-19 20:41:56 +0530

    debugged login and register endpoints

--- backend/routers/user.routes.js
+++ backend/routers/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { User } from '../models/users.model.js'
-import bcrypt from 'bcrypt'
+import bcrypt, { hash } from 'bcrypt'
 import { upload } from '../middlewares/multer.js'
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import jwt from 'jsonwebtoken'
@@ -14,67 +14,80 @@ router.post('/register', upload.fields([{ name: 'profilePicture', maxCount: 1 }]
     const hashedPassword = await bcrypt.hash(password, 5)
 
     if ([username, password, email, profilePicture].some(field => field?.trim() === '')) {
-        throw new Error('All Fields Are Required')
+        res.status(404).json({
+            ErrorMessage: 'All Fields Are Required'
+        })
     }
 
     const existedUser = await User.findOne({
-        $or: [{ username }, { email }]
+        username,
+        email
     })
 
     if (existedUser) {
-        throw new Error('User Already Exists')
+        res.status(404).json({
+            ErrorMessage: 'User Already Exists'
+        })
     }
 
     const imageLocalPath = req.files?.profilePicture[0]?.path
 
     if (!imageLocalPath) {
-        throw new Error('Profile Picture Is Required')
+        res.status(404).json({
+            ErrorMessage: 'Profile Picture Is Required'
+        })
     }
 
     const picture = await uploadOnCloudinary(imageLocalPath)
     
     if (!picture) {
-        throw new Error('Profile Picture Is Required')
+        res.status(404).json({
+            ErrorMessage: 'There Was An Error Uploading The Picture'
+        })
     }
 
     try {
         const createUser = await User.create({ username: username.toLowerCase(), password: hashedPassword, email, profilePicture: picture.url })
-        console.log('User registered')
         res.json({
             Message: 'User Created Successfully'
         })
     } catch (err) {
-        console.log('ERROR Adding User In The Database/Register: ', err)
-        throw new Error('User Not Added In The Database')
+        res.status(404).json({
+            ErrorMessage: 'Error Adding User In The Database/Register' 
+        })
     }
-
-    console.log('Files', req.files)
-    console.log('Body', req.body)
 })
 
 router.post('/login', async (req, res) => {
     const { username, password, email } = req.body
-    if ([username, password, email].some(field => field?.trim() === '')) {
-        throw new Error('All Fields Are Required')
-    }
-    const hashedPassword = await bcrypt.compare(password)
-    if (hashedPassword) {
+    if ([username, password, email].some((field) => field.trim() === '' || undefined)) {
         res.status(404).json({
-            ErrorMessage: 'Password Is Incorrect'
+            ErrorMessage: 'All Fields Are Required'
         })
-    }
-    try {
-        const user = await User.findOne({
-            $or: [{ username }, { password }, { email }]
-        })
-        if (user) {
-            const token = jwt.sign({ username, email }, process.env.JWT_SECRET)
-            localStorage.setItem('token', token)
-            console.log(token)
-        } else {
-            throw new Error('User Not Registered')
-        }
-    } catch (err) {
-        throw new Error('Error Fetching Data From Database')
+    } else {
+        try {
+            const user = await User.findOne({
+                username,
+                email
+            })
+            const hashedPassword = await bcrypt.compare(password, user.password)
+
+            if (hashedPassword) {
+                const token = jwt.sign({ username, email }, process.env.JWT_SECRET)
+                console.log(token)
+                res.json({
+                    Message: "User Logged In Successfully"
+                })
+            } else {
+                res.status(404).json({
+                    ErrorMessage: "User Not Registered"
+                })
+            }
+        } catch (err) {
+            res.status(404).json({
+                ErrorMessage: `User Not Exists: Register, ${err}`
+            })
+        }   
     }
 })
+
